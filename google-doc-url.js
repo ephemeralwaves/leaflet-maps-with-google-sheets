28c61e39c04ebe87e0b8c23cd6aea0ab2c75e0ca
ephemeralwaves <gjhunjhn@gmail.com>
2020-03-21 15:33:29 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bBwCp9oQY6jYMx4mOLNeoB1hiVw8CmqESGPitIizYME/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bBwCp9oQY6jYMx4mOLNeoB1hiVw8CmqESGPitIizYME/edit#gid=0';
